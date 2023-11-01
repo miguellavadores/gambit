193a6173003d44a56c800cc66487e6b2e5c8a504
Miguel Angel Lavadores <miguel.lavadores@grupofiglobal.com>
2023-11-01 08:12:58 -0600

    Ultimo Commit

--- auth/auth.go
+++ auth/auth.go
@@ -9,16 +9,16 @@ import (
 )
 
 type TokenJSON struct {
-	Sub       string
-	Event_Id  string
-	Token_use string
-	Scope     string
-	Auth_time int
-	Iss       string
-	Exp       int
-	Iat       int
-	Client_id string
-	Username  string
+	Sub        string
+	Event_Id   string
+	Token_user string
+	Scope      string
+	Auth_time  int
+	Iss        string
+	Exp        int
+	Iat        int
+	Client_id  string
+	Username   string
 }
 
 func ValidoToken(token string) (bool, error, string) {
