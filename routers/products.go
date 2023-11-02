3bd579bccfd8cb977676dd3b78eeece14b68bd76
Miguel Angel Lavadores <miguel.lavadores@grupofiglobal.com>
2023-11-02 12:17:17 -0600

    Ultimo Commit

--- routers/products.go
+++ routers/products.go
@@ -88,7 +88,7 @@ func SelectProduct(request events.APIGatewayV2HTTPRequest) (int, string) {
 	orderType = param["orderType"]
 	orderField = param["orderField"]
 
-	if !strings.Contains("ITDFPCS", orderType) {
+	if !strings.Contains("ITDFPCS", orderField) {
 		orderField = ""
 	}
 
